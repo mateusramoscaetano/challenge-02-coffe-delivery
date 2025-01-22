ec03029b2793f02d405ba5ba12acdc896ebeeb78
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-01-22 08:04:30 -0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <h1>hello coffee delivery</h1>
+      </ThemeProvider>
+    </>
+  );
+}
